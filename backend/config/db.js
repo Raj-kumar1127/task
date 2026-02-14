ead5e4a9f402d573c1792a153b1db2688f0c8cec
Raj-kumar1127 <rajkumar933569@gmail.com>
2026-02-14 09:43:43 +0530

    Change MongoDB connection string in db.js
    Updated MongoDB connection string to use a specific URI.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect("mongodb+srv://rajc07168_db_user:rkA4nWF4yyFBX3rm@cluster0.ct2tdv8.mongodb.net/?appName=Cluster0", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
