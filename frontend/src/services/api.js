3b44d4ebb772d2589c5f575082bf05faeabf43a8
Raj-kumar1127 <rajkumar933569@gmail.com>
2026-02-14 10:15:29 +0530

    Update API base URL for axios instance

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://community1-aps1.onrender.com/api",
+  baseURL: "https://community1-aps1.onrender.com/",
 });
 
 API.interceptors.request.use((req) => {
